7b8bc74fa6072f523470c21ece91156b7566b8d2
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-09-30 12:50:27 -0300

    Conexão com o wi-fi

---
+++ S4/S4/S4.ino
@@ -0,0 +1,19 @@
+#include <WiFi.h>
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+void setup() {
+  Serial.begin(115200); // configura a placa para mostrar na tela
+  WiFi.begin(SSID, PASS); // tenta conectar na rede
+  Serial.println("Conectando no WiFi");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.println(".");
+    delay(200);
+  }
+  Serial.println("Conectado com sucesso!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
