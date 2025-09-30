0723f41a5a85e956891e8912898f08dbde88657c
miguel-paes <miguelpaesdasilva@gmail.com>
2025-09-30 12:43:52 -0300

    Código de conexão WiFi

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,11 +1,20 @@
 //ARQUIVO S1 - MIGUEL PAES DA SILVA - SMART RAIL (SR)
 
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
 
+void setup() {
+  Serial.begin(115200); //Configura a placa para mostrar na tela
+  WiFi.begin(SSID, PASS); //Tenta conectar na rede
+  Serial.println("Conectando ao WiFI");
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
+  }
+    Serial.println("Conectado com Sucesso!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
