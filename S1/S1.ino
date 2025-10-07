1aef8e6921b59cf07fc3bd18aff8da2c27d401a5
miguel-paes <miguelpaesdasilva@gmail.com>
2025-10-07 12:24:19 -0300

    Conexão com o Broker

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,8 +1,19 @@
 //ARQUIVO S1 - MIGUEL PAES DA SILVA - SMART RAIL (SR)
 
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client; //Cria objeto para wifi
+PubSubClient mqtt(client); //Cria objeto para MQTT usando wifi
+
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
+
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = "";
+const String brokerPass = "";
 
 void setup() {
   Serial.begin(115200); //Configura a placa para mostrar na tela
@@ -12,7 +23,20 @@ void setup() {
     Serial.print(".");
     delay(200);
   }
-    Serial.println("Conectado com Sucesso!");
+    Serial.println("\nConectado com Sucesso!");
+
+    mqtt.setServer(brokerURL.c_str(), brokerPort);
+    Serial.println("Conectando no Broker");
+
+    String boardID = "S1-";
+
+    boardID += String(random(0xffff), HEX);
+    
+    while(!mqtt.connect(boardID.c_str())) {
+      Serial.println(".");
+      delay(200);
+    }
+    Serial.println("\nConectado com sucesso ao Broker");
 }
 
 void loop() {
