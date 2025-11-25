8c6d67c920141d1649fef7f19dc5e608cf06feff
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-11-25 11:43:16 -0300

    modifiquei alguns comentarios

--- S3/S3.ino
+++ S3/S3.ino
@@ -7,11 +7,11 @@
 #include "env.h"
 
 WiFiClientSecure wifiClient;
-WiFiClient client;         // cria o objeto para o wifi
-PubSubClient mqtt(client);// cria o objeto para o mqtt usando wifi
+WiFiClient client;          // cria o objeto para o wifi
+PubSubClient mqtt(client);  // cria o objeto para o mqtt usando wifi
 
-Servo meuServo1; // cria um objeto do tipo servo
-Servo meuServo2; 
+Servo meuServo1;  // cria um objeto do tipo servo
+Servo meuServo2;
 
 // declaração de variaveis
 const byte ldr = A0;
@@ -23,74 +23,67 @@ int valorLdr = 0;
 float duracao, distancia;
 
 // declaração de funções
-void controladorLedsPresenca(float distance){
-	if(distance > 1000){
-		digitalWrite(ledVerde, HIGH);
+void controladorLedsPresenca(float distance) {
+  if (distance > 1000) {
+    digitalWrite(ledVerde, HIGH);
     digitalWrite(ledVermelho, LOW);
-    delay(1000);
-	} else if(distance < 1000){
+  } else if (distance < 1000) {
     digitalWrite(ledVerde, LOW);
     digitalWrite(ledVermelho, HIGH);
-    delay(1000);
   } else {
     digitalWrite(ledVerde, LOW);
     digitalWrite(ledVermelho, LOW);
-    delay(1000);
   }
 }
 
-void callback(char* topic, byte* payload, unsigned long length){
+void callback(char* topic, byte* payload, unsigned long length) {
   String mensagemRecebida = "";
-  for(int i = 0; i < length; i++){
-    mensagemRecebida += (char) payload[i];
+  for (int i = 0; i < length; i++) {
+    mensagemRecebida += (char)payload[i];
   }
   byte int velocidade = mensagemRecebida.toInt();
   Serial.println(mensagemRecebida);
-  // fazer o controle de leds aqui
+  // controle dos leds
   controladorLeds(distance);
 }
 
-void calcularDistanciaDuracao(float distance, float duration)
-{
-	duration = pulseIn(pinoEcho, HIGH);
-  distance = (duration*0.343)/2;
+void calcularDistanciaDuracao(float distance, float duration) {
+  duration = pulseIn(pinoEcho, HIGH);
+  distance = (duration * 0.343) / 2;
   Serial.println("Distancia: ");
   Serial.println(distance);
-	delay(100);
+  delay(100);
 }
 
-void atuadorServo1() 
-{
-	
-  	// mover o servo motor para 0 graus
-  	meuServo1.write(0);
-  	delay(1000);
-  	// mover o servo para 180 graus
-  	meuServo1.write(180);
-  	delay(1000);
-  	// retorna para a posição inicial
-  	meuServo1.write(90);
-  	delay(1000);
+void atuadorServo1() {
+  // mover o servo motor para 0 graus
+  meuServo1.write(0);
+  delay(1000);
+  // mover o servo para 180 graus
+  meuServo1.write(180);
+  delay(1000);
+  // retorna para a posição inicial
+  meuServo1.write(90);
+  delay(1000);
 }
 
-void atuadorServo2()
-{
-  	// mover o servo motor para 0 graus
-  	meuServo2.write(0);
-  	delay(1000);
-  	// mover o servo para 180 graus
-  	meuServo2.write(180);
-  	delay(1000);
-  	// retorna para a posição inicial
-  	meuServo2.write(90);
-  	delay(1000);
+void atuadorServo2() {
+  // mover o servo motor para 0 graus
+  meuServo2.write(0);
+  delay(1000);
+  // mover o servo para 180 graus
+  meuServo2.write(180);
+  delay(1000);
+  // retorna para a posição inicial
+  meuServo2.write(90);
+  delay(1000);
 }
 
 // funcao para conectar wifi
-void conectarWifi(){
+void conectarWifi() {
   Serial.println("Conectando no WiFi");
-  // enquanto não estiver conectado no wifi mostra "." e liga o led amarelo
-  while(WiFi.status() != WL_CONNECTED){
+ // Loop para conectar no wifi
+  while (WiFi.status() != WL_CONNECTED) {
     ledcWrite(ledVermelho, 150);
     ledcWrite(ledVerde, 150);
     ledcWrite(ledAzul, 0);
@@ -100,31 +93,31 @@ void conectarWifi(){
 }
 
 // funcao configurar servidor do broker/porta
-String configurarServidorBroker(){
+String configurarServidorBroker() {
   // configura o servidor do broker/porta
   mqtt.setServer(BROKER_URL, BROKER_PORT);
   Serial.println("Conectando no Broker");
-  
-  // cria um nome que começa com s4-
-  String userID = "S3-RUBENS"; 
 
-  // junta o "s4-" com um numero aleatorio hexadecimal
+  // cria um nome que começa com s3-
+  String userID = "S3-RUBENS";
+
+  // junta o "s3-" com um numero aleatorio hexadecimal
   userID += String(random(0xffff), HEX);
   return userID;
 }
 
 // funcao para conectar broker
-void conectarBroker(){
+void conectarBroker() {
   String userID = configurarServidorBroker();
-  // enquanto não estiver conectado mostra "."
-  while(!mqtt.connected()){
-    if(mqtt.connect(userID.c_str(), BROKER_USR_NAME, BROKER_USR_PASS)){
+  // Loop para conectar no broker
+  while (!mqtt.connected()) {
+    if (mqtt.connect(userID.c_str(), BROKER_USR_NAME, BROKER_USR_PASS)) {
       Serial.print('.');
       ledcWrite(ledVermelho, 150);
       ledcWrite(ledAzul, 150);
       ledcWrite(ledVerde, 0);
       delay(2000);
-    } else { // caso dê erro de conexao com broker
+    } else {  // caso dê erro de conexao com broker
       ledcWrite(ledVermelho, 255);
       ledcWrite(ledAzul, 0);
       ledcWrite(ledVerde, 0);
@@ -135,42 +128,40 @@ void conectarBroker(){
 
 //codigo principal
 
-void setup()
-{
+void setup() {
   // define os pinos dos leds
   pinMode(ledVermelho, OUTPUT);
   pinMode(ledVerde, OUTPUT);
   pinMode(ledAzul, OUTPUT);
 
-  meuServo1.attach(pinoServo1); // associa o objeto Servo ao pino
+  meuServo1.attach(pinoServo1);  // associa o objeto Servo ao pino
   meuServo2.attach(pinoServo2);
   pinMode(pinoTrig, OUTPUT);
   pinMode(pinoEcho, INPUT);
-  
+
   // define os leds como entrada analogico
   ledcAttach(ledVermelho, 5000, 8);
   ledcAttach(ledVerde, 5000, 8);
   ledcAttach(ledAzul, 5000, 8);
-	
+
   //configuração do wifi
   wifiClient.setInscure();
-  Serial.begin(115200); // configura a placa para mostrar na tela
-  WiFi.begin(SSID, PASS); // tenta conectar na rede
-  
+  Serial.begin(115200);    // configura a placa para mostrar na tela
+  WiFi.begin(SSID, PASS);  // tenta conectar na rede
+
   //  conectando no wifi
   conectarWifi();
 
   // conectando no broker
   conectarBroker();
 
-  mqtt.subscribe(topico.c_str()); // inscrever em um topico
+  mqtt.subscribe(topico.c_str());  
   mqtt.subscribe("miguel123");
   mqtt.setCallback(callback);
   Serial.println("Conectado com sucesso!");
 }
 
-void loop()
-{
+void loop() {
   calcularDistanciaDuracao(distancia, duracao);
   controladorLedsPresenca(distancia);
   atuadorServo1();
