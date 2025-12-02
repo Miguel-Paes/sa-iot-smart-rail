2ff373755d14adfc4274d05fd482cd264c1c1f6a
Nathan da Cunha <nathan_cunha@estudante.sesisenai.org.br>
2025-12-02 07:54:50 -0300

    Configure MQTT broker connection parameters
    Added MQTT broker connection details.

--- S4/S4.ino
+++ S4/S4.ino
@@ -7,6 +7,12 @@ WiFiClientSecure wifiClient;
 WiFiClient client;      
 PubSubClient mqtt(client);
 
+//Conexão
+const char *mqtt_broker = "1acd7ac2686b4748913cce80ad4161df.s1.eu.hivemq.cloud";
+const int mqtt_port = 8883; // Porta padrão
+const char *mqtt_user = "user_placa_4";
+const char *mqtt_password = "Userplaca4";
+
 // topicos
 const String topico = "SmartRail/S4/Trem/Vel"; 
 const String topico2 = "SmartRail/S4/Trem/Controle_led_rgb_s4";
